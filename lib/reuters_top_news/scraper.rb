4d1cfa413a66a5b7f611e593191636bc27ab0b59
Felix Lemerle <felix@lemerle.fr>
2018-07-22 15:10:25 -0700

    scrape_articles coming along

--- lib/reuters_top_news/scraper.rb
+++ lib/reuters_top_news/scraper.rb
@@ -1,15 +1,17 @@
 class ReutersTopNews::Scraper
+
   def get_page
     Nokogiri::HTML(open("https://www.reuters.com/"))
   end
 
-#:title, :authors, :time, :url, :text
   def scrape_articles
-    title = self.get_page.css("h2.story-title")[0].children[1].children[0].text
     url = self.get_page.css("h2.story-title")[0].children[1].attributes["href"].value
     page = Nokogiri::HTML(open("https://www.reuters.com" + url))
-    time = page.css("div.date_V9eGk")[0].children[0].text
-    authors_array = page.css("div.byline_31BCV")[0].children
+
+    title = page.css("h1.ArticleHeader_headline")[0].children[0].text
+    time = page.css("div.ArticleHeader_date")[0].children[0].text
+
+    authors_array = page.css("div.BylineBar_byline")[0].children
     authors = ""
     authors_array.each do |author|
       if authors != ""
@@ -17,6 +19,14 @@ class ReutersTopNews::Scraper
       end
       authors += author.children[0].children[0].text
     end
-#    text = page.css
+
+    text_array = page.css("div.StandardArticleBody_body")[0].children
+    text = []
+    text_array.each do |element|
+      if element.name == "p"
+        text << element.children[0].text
+      end
+    end
   end
+
 end
